822adff7bfe1dd4974fbfd4563bdde2bff6d9521
wly <wlywly0735@126.com>
2023-10-21 21:10:01 +0800

    Distributed_Lock_self_developed

---
+++ src/main/java/com/wly/redis/mylock/DistributedLockFactory.java
@@ -0,0 +1,41 @@
+package com.wly.redis.mylock;
+
+import cn.hutool.core.util.IdUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.locks.Lock;
+
+@Component
+public class DistributedLockFactory
+{
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    private String lockName;
+    private String uuid;
+
+    public DistributedLockFactory()
+    {
+        this.uuid = IdUtil.simpleUUID();
+    }
+
+    public Lock getDistributedLock(String lockType)
+    {
+        if(lockType == null) return null;
+
+        if(lockType.equalsIgnoreCase("REDIS")){
+            this.lockName = "zzyyRedisLock";
+            return new RedisDistributedLock(stringRedisTemplate,lockName,uuid);
+        }else if(lockType.equalsIgnoreCase("ZOOKEEPER")){
+            this.lockName = "zzyyZookeeperLockNode";
+            //TODO zookeeper版本的分布式锁
+            return null;
+        }else if(lockType.equalsIgnoreCase("MYSQL")){
+            //TODO MYSQL版本的分布式锁
+            return null;
+        }
+
+        return null;
+    }
+}
